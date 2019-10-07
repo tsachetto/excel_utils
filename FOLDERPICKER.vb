9255e83d7c5ff7f6cc5e42d0325fcdf9a8c64589
thomaz sachetto <thomazsachetto@gmail.com>
2019-10-07 16:42:19 -0300

    Update FOLDERPICKER.vb

--- FOLDERPICKER.vb
+++ FOLDERPICKER.vb
@@ -1,15 +1,20 @@
-Function GetFolder() As String
-    Dim fldr As FileDialog
-    Dim sItem As String
-    Set fldr = Application.FileDialog(msoFileDialogFolderPicker)
+Dim fldr As FileDialog
+Dim diretorio as String
+
+Set fldr = Application.FileDialog(msoFileDialogFolderPicker)
+
     With fldr
-        .Title = "Select a Folder"
+        .Title = "Escolha um diretório:"
         .AllowMultiSelect = False
-        .InitialFileName = Application.DefaultFilePath
         If .Show <> -1 Then GoTo NextCode
         sItem = .SelectedItems(1)
     End With
+    
 NextCode:
-    GetFolder = sItem
+
+    If sItem = "" Then
+        Exit Sub
+    Else: End If
+    
+    diretorio = sItem & "\"
     Set fldr = Nothing
-End Function
